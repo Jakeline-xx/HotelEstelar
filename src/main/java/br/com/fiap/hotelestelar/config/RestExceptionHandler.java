9d69d4d3372ea5d0ff6e711c1bfaf5b1a8257463
milenaggoes <rm94305@fiap.com.br>
2023-09-04 14:36:37 -0300

    profiles

--- src/main/java/br/com/fiap/hotelestelar/config/RestExceptionHandler.java
+++ src/main/java/br/com/fiap/hotelestelar/config/RestExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.fiap.hotelestelar.models.RestError;
 import br.com.fiap.hotelestelar.models.RestValidationError;
 @RestControllerAdvice
 public class RestExceptionHandler {
